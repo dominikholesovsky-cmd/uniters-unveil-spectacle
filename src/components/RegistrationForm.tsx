92b1e5b24181aeff6866ecf39c0c5077cc0953a2
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2025-11-05 21:23:05 +0100

    Update RegistrationForm.tsx

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -152,7 +152,7 @@ const RegistrationForm = ({ language }: RegistrationFormProps) => {
 
   const handleNavigationClick = () => {
     const coordinates = "49.1956718,16.5913221";
-    const mapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${coordinates}`;
+    const mapsUrl = const mapsUrl = `https://www.google.com/maps/@${coordinates}`;
     window.open(mapsUrl, "_blank");
   };
 
