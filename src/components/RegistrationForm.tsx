65b32c070634a424321aaa11b73444827fc51137
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2025-11-05 15:27:47 +0100

    Update RegistrationForm.tsx

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -10,7 +10,7 @@ import { useToast } from "@/hooks/use-toast";
 import { CheckCircle2, Navigation } from "lucide-react";
 
 // Power Automate endpoint URL
-const POWER_AUTOMATE_URL = "https://default54b8b3209661409e9b3e7fc3e0adae.a5.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/7e4728fa129c4a869c877437c791fcea/triggers/manual/paths/invoke?api-version=1";
+const POWER_AUTOMATE_URL = "https://default54b8b3209661409e9b3e7fc3e0adae.a5.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/7e4728fa129c4a869c877437c791fcea/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Ae_Ysv7Bovz-dFpy-KNXpk5dRI8nM_HBi6WYL46drPA";
 
 interface RegistrationFormProps {
   language: "cs" | "en";
