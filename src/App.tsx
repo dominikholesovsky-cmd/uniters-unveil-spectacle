0a6f4d304c881a9ac7eb81276f3df069a0eb3a5a
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2025-11-05 16:52:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import BackToTop from "./components/BackToTop";
-import Terms from "./components/Terms";
+import Terms from "./components/TermsModal";
 
 
 const queryClient = new QueryClient();
