b512f57f0d4e64f9fff39c8405160dfc21f1a3d7
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2025-11-05 13:17:34 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -150,8 +150,8 @@ const Hero = ({ language, onRegisterClick }: HeroProps) => {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-6 sm:bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce hidden sm:block">
-        <div className="w-5 h-8 sm:w-6 sm:h-10 border-2 border-white/50 rounded-full flex items-start justify-center pt-24 p-2">
+      <div className="absolute bottom-6 sm:bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce pt-12 hidden sm:block">
+        <div className="w-5 h-8 sm:w-6 sm:h-10 border-2 border-white/50 rounded-full flex items-start justify-center p-2">
           <div className="w-1 h-2 sm:h-3 bg-white/50 rounded-full" />
         </div>
       </div>
