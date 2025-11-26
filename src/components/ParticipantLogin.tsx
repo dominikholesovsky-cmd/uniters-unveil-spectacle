10f8b72254281c7b29348b3bc34917d7e27e199a
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2025-11-26 16:29:04 +0100

    Update ParticipantLogin.tsx

--- src/components/ParticipantLogin.tsx
+++ src/components/ParticipantLogin.tsx
@@ -242,7 +242,7 @@ export default function ParticipantLogin({ language = "cs" }: ParticipantLoginPr
                     </div>
                     {p.id !== session.user.id ? (
                       <Button
-                        className="bg-foreground hover:bg-gray-100 transition-colors"
+                        className="bg-foreground hover:bg-muted-foreground transition-colors"
                         onClick={() => startChat(p)}
                         size="sm"
                       >
