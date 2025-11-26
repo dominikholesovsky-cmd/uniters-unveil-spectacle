1570c69ca17fb56b8482984afb3a792916f72127
dominikholesovsky-cmd <dominik.holesovsky@gmail.com>
2025-11-26 20:58:19 +0100

    X

--- src/components/ParticipantLogin.tsx
+++ src/components/ParticipantLogin.tsx
@@ -435,11 +435,6 @@ export default function ChatButtonAndModal({ language = "cs" }: ParticipantLogin
                             {/* Titulek se mění podle stavu */}
                             {session ? t.listTitle : t.loginTitle}
                         </DialogTitle>
-                        <DialogClose asChild>
-                            <Button variant="ghost" className="absolute right-4 top-4 text-gray-500 hover:text-gray-800">
-                                <X className="w-5 h-5" />
-                            </Button>
-                        </DialogClose>
                     </DialogHeader>
 
                     {/* Obsah Modalu - Přihlášení, Seznam nebo Chat */}
